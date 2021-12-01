93b1859ea65c498e4c42779e8449752d9dd1d864
Ashley Mattam <amattam427@gmail.com>
2021-12-01 15:10:21 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+console.log(callback()); 
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+    }
+  }
+  
+  
+  function returnsAnAnonymousFunction(){
+    return function(){
+    }
+  }
